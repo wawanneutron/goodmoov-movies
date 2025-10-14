609130e9b42949677cb5b4574cb025c75d931abf
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:47:59 +0700

    feat: skeleton carousel

---
+++ components/CarouselSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative overflow-hidden rounded-2xl mt-8">
+    <div class="flex animate-pulse">
+      <div
+        class="min-w-full flex-shrink-0 bg-gray-700/40 h-56 md:h-96 rounded-2xl"
+      ></div>
+    </div>
+
+    <div class="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-3">
+      <div
+        v-for="n in 4"
+        :key="n"
+        class="w-3 h-3 rounded-full bg-gray-500/60"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
