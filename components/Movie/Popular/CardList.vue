6caa713dc1bd1e6fe25f53ffaee8763ffb34dbec
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:44:57 +0700

    feat: card list popular movies

---
+++ components/Movie/Popular/CardList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <SectionTitle class="w-12" title="Popular" />
+    <ScrollContainer>
+      <MovieCardSkeleton
+        v-if="loading"
+        v-for="n in popularMovies.length"
+        :key="n"
+      />
+
+      <MovieCard
+        v-else
+        v-for="(popular, idx) in popularMovies"
+        :movie="popular"
+        :key="idx"
+      />
+    </ScrollContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const popularStore = usePopularMovieStore()
+const { fetchPopularMovies } = popularStore
+const { popularMovies, loading, error } = storeToRefs(popularStore)
+
+onMounted(async () => {
+  await fetchPopularMovies()
+})
+</script>
+
+<style></style>
