2ed805d3b1332ba8130b5d3edd8c42d18807c896
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:45:34 +0700

    feat: navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="bg-[var(--theme-primary)] flex justify-between p-4 rounded-b-sm">
+    <Logo />
+    <DarkMode />
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
