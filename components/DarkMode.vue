d7a43fe8321adb71286e96a153d7bdae903e0a7b
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:46:07 +0700

    feat: dark mode component

---
+++ components/DarkMode.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button
+      @click="toggleTheme"
+      class="p-2 bg-[var(--theme-primary)] text-[var(--theme-text)] rounded cursor-pointer"
+      :title="isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode'"
+    >
+      <Icon
+        :name="isDark ? 'ph:sun-duotone' : 'ph:moon-duotone'"
+        size="24"
+        class="text-[var(--theme-text)]"
+      />
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const isDark = ref<boolean>(false)
+
+onMounted(() => {
+  const savedTheme = localStorage.getItem('theme')
+  if (savedTheme === 'dark') {
+    document.documentElement.classList.add('dark')
+    isDark.value = true
+  }
+})
+
+function toggleTheme() {
+  const html = document.documentElement
+  const isTogleDark = html.classList.toggle('dark')
+  localStorage.setItem('theme', isTogleDark ? 'dark' : 'light')
+  isDark.value = !!isTogleDark
+}
+</script>
+
+<style></style>
