99ff48ca8c844c1fb7c16d3387d0746cb249ac8e
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:49:58 +0700

    feat: typescript

---
+++ types/movie.d.ts
@@ -0,0 +1,113 @@
+export interface Genre {
+  id: number
+  name: string
+}
+
+export interface Movie {
+  adult: boolean
+  backdrop_path: string | null
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string | null
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface ProductionCompany {
+  id: number
+  logo_path: string | null
+  name: string
+  origin_country: string
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string
+  name: string
+}
+
+export interface SpokenLanguage {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
+export interface Cast {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path: string | null
+  cast_id: number
+  character: string
+  credit_id: string
+  order: number
+}
+
+export interface Crew {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path: string | null
+  credit_id: string
+  department: string
+  job: string
+}
+
+export interface Credits {
+  cast: Cast[]
+  crew: Crew[]
+  total_pages?: number
+  total_results?: number
+}
+
+export interface recommendations {
+  page: number
+  results: Movie[]
+  total_pages: number
+  total_results: number
+}
+
+export interface MovieDetail {
+  adult: boolean
+  backdrop_path: string | null
+  belongs_to_collection: any | null
+  budget: number
+  genres: Genre[]
+  homepage: string | null
+  id: number
+  imdb_id: string | null
+  origin_country: string[]
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string | null
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  release_date: string
+  revenue: number
+  runtime: number | null
+  spoken_languages: SpokenLanguage[]
+  status: string
+  tagline: string | null
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+  credits?: Credits
+  recommendations: recommendations
+}
