d9d0e366cdbf875ecaed86845dce13dd285326f2
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:48:13 +0700

    feat: skeleton card movie

---
+++ components/Movie/CardSkeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block w-44 mt-2">
+    <div
+      class="overflow-hidden rounded relative bg-[var(--theme-skeleton-bg)] animate-pulse"
+    >
+      <div class="w-44 h-64 bg-gray-700/40"></div>
+      <div
+        class="absolute bottom-0 right-0 bg-gray-600/50 rounded text-transparent text-sm p-2"
+      >
+        --
+      </div>
+    </div>
+
+    <div class="pt-2 space-y-1">
+      <div class="h-4 bg-gray-600/50 rounded w-3/4 animate-pulse"></div>
+      <div class="h-3 bg-gray-600/40 rounded w-1/3 animate-pulse"></div>
+    </div>
+  </div>
+</template>
