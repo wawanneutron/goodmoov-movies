b0988f6e92c9c246f9fb5cbab6eacb80bf66d8b8
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:47:10 +0700

    feat: title reusabe component

---
+++ components/SectionTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inline-block">
+    <div class="border-t-3 border-[var(--theme-secondary-light)]"></div>
+    <h2 class="text-[var(--theme-text)] text-xl font-bold">
+      {{ title }}
+    </h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string
+}>()
+</script>
+
+<style></style>
