e96586e6816c1d4ae935ee5133d621634bacecc4
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:45:47 +0700

    feat: reusable logo

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-link href="/" class="text-2xl font-bold">
+    <span class="text-[var(--theme-secondary-light)]">good</span>
+    <span
+      class="border-b-2 border-[var(--theme-secondary-light)] text-[var(--theme-text)]"
+      >moov</span
+    >
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
