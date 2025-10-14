f5126558dfbd67e8086d8173afd05b4fa22c29f9
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:44:37 +0700

    feat: card list latest movies

---
+++ components/Movie/Latest/CardList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <SectionTitle class="w-8" title="Latest" />
+    <ScrollContainer>
+      <MovieCardSkeleton
+        v-if="loading"
+        v-for="n in latestMovies.length"
+        :key="n"
+      />
+
+      <MovieCard
+        v-else
+        v-for="(latest, idx) in latestMovies"
+        :movie="latest"
+        :key="idx"
+      />
+    </ScrollContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const latestStore = useLatestMovieStore()
+const { fetchLatestMovies } = latestStore
+const { latestMovies, loading, error } = storeToRefs(latestStore)
+
+onMounted(async () => {
+  await fetchLatestMovies()
+})
+</script>
+
+<style></style>
