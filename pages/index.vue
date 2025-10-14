bba55642de278bbfd4effe39b967a2f21012cc94
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:49:01 +0700

    feat: home pages

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="space-y-6">
+    <Carousel />
+    <MovieGenreCardList />
+    <MoviePopularCardList />
+    <MovieLatestCardList />
+  </section>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
