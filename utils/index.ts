0ea6e2c1c1d1df030d5e04a3175a3e3291f6b08d
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:50:50 +0700

    feat: utils

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+export const getImageUrl = (
+  path: string | null,
+  size: 'w500' | 'w1280' = 'w500'
+) => {
+  return path ? `https://image.tmdb.org/t/p/${size}${path}` : '/no-image.png'
+}
+
+export function formatRating(rating: number): string {
+  return (Math.round(rating * 10) / 10).toFixed(1)
+}
+
+export function getYear(releaseDate?: string): string {
+  if (!releaseDate) return 'N/A'
+  return releaseDate.split('-')[0]
+}
+
+export function formatRuntime(minutes: number | null): string {
+  if (!minutes || minutes === 0) return '-'
+  const h = Math.floor(minutes / 60)
+  const m = minutes % 60
+  return `${h}h ${m}m`
+}
