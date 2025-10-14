f08482928e6056a3db4c138e87c2ab6183db24c2
wawanneutron <wawanneutron1331@gmail.com>
2025-10-14 20:48:26 +0700

    feat: skeleton card genre

---
+++ components/Movie/Genre/CardSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex items-end justify-end rounded bg-gray-600/40 w-36 h-20 p-2 animate-pulse"
+  >
+    <div class="w-16 h-3 bg-gray-500/60 rounded"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
